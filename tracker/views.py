05d2d85778e2e0dad379914dc3a19e81705ce91a
Archana R <archanarkrishna1@gmail.com>
2026-01-29 09:59:40 +0530

    water intake site

--- tracker/views.py
+++ tracker/views.py
@@ -70,7 +70,7 @@ from django.core.paginator import Paginator
 @login_required
 def intake_list(request):
     intakes = WaterIntake.objects.filter(user=request.user).order_by('-date')
-    paginator = Paginator(intakes, 5)  # 5 records per page
+    paginator = Paginator(intakes, 5)  
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
